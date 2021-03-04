27597e3207683d9dc66a33fec0daeeaeb47e87fa
Tiago Castro <tiagolobocastro@gmail.com>
2021-03-04 17:12:50 +0000

    chore(repo): add control-plane to its own repo
    Added the mayastor control-plane.
    Refactored the nix shell and derivations.

---
+++ default.nix
@@ -0,0 +1,10 @@
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs {
+    overlays = [
+      (_: _: { inherit sources; })
+      (import ./nix/overlay.nix)
+    ];
+  };
+in
+pkgs
