9bd1013b4246d8c3fc661abdb9a2fc844bf70a46
Tiago Castro <tiagolobocastro@gmail.com>
2021-04-08 10:46:28 +0100

    chore: update mayastor
    Update mayastor to 2868e83704177e439d7bfb4cbd94cff5a371db91 (current
     tip of develop branch)

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -6,7 +6,7 @@ self: super: {
     repo = "Mayastor";
     # Use rev from the RPC patch in the workspace's Cargo.toml
     rev = (builtins.fromTOML (builtins.readFile ../Cargo.toml)).patch.crates-io.rpc.rev;
-    sha256 = "0sh8rnmbkxfg36pgsp060kyk83nwqlhs4fxzasq3zk5bn4mqhs47";
+    sha256 = "1qdr9aj3z5jpbdrzqdxkh3ga98wq9ivsr5qrc1g6n0j9w5pjk2ry";
   };
   control-plane = super.callPackage ./pkgs/control-plane { };
 }
