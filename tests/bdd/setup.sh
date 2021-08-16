0dc7c6b49404c4368a34b38f0c73db2e8d3cf6e8
Paul Yoong <paul.yoong@mayadata.io>
2021-08-16 19:50:39 +0100

    chore: fixup setup.sh

--- tests/bdd/setup.sh
+++ tests/bdd/setup.sh
@@ -2,7 +2,7 @@
 
 DIR_NAME="$(dirname "$0")"
 
-virtualenv --no-setuptools tests/bdd/venv
+virtualenv --no-setuptools "$DIR_NAME"/venv
 source "$DIR_NAME"/venv/bin/activate
 pip install -r "$DIR_NAME"/requirements.txt
 export PYTHONPATH=$PYTHONPATH:$DIR_NAME/openapi
