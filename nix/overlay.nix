276bb3dc2eac76b90c3956db4779c75757d3034b
Blaise Dias <blaise.dias@mayadata.io>
2021-11-01 21:29:39 +0000

    fix: remove unnecessary directive

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -3,5 +3,4 @@ self: super: {
   control-plane = super.callPackage ./pkgs/control-plane { };
   utils = super.callPackage ./pkgs/utils { };
   openapi-generator = super.callPackage ./pkgs/openapi-generator { };
-  control-plane-cov = (super.callPackage ./pkgs/control-plane { }).cov;
 }
