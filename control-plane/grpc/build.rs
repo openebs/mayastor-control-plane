de4bcc605066b20ba04a9125e2f623ac2180dae1
Abhinandan-Purkait <abhinandan.purkait@mayadata.io>
2022-01-25 21:28:52 +0530

    refactor(replace nats): add versioned structure and proto for replica
    Signed-off-by: Abhinandan-Purkait <abhinandan.purkait@mayadata.io>

--- control-plane/grpc/build.rs
+++ control-plane/grpc/build.rs
@@ -3,7 +3,11 @@ extern crate tonic_build;
 fn main() {
     tonic_build::configure()
         .compile(
-            &["proto/v1/pool/pool.proto", "proto/v1/misc/common.proto"],
+            &[
+                "proto/v1/pool/pool.proto",
+                "proto/v1/replica/replica.proto",
+                "proto/v1/misc/common.proto",
+            ],
             &["proto/"],
         )
         .unwrap();
