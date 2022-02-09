4fbfef7efb862bf426e9f236eea45d45722542d6
Tiago Castro <tiagolobocastro@gmail.com>
2022-02-09 16:44:40 +0000

    feat: new base grpc client that supports adding timeouts per request
    This can be used as a inner helper which provides common functionality of creating a new client
    connection and setting specific timeouts for each request.
    This allows us to use long-lived connections with keep-alives.

--- control-plane/grpc/src/grpc_opts/mod.rs
+++ control-plane/grpc/src/grpc_opts/mod.rs
@@ -1,5 +1,10 @@
 use common_lib::{mbus_api::TimeoutOptions, types::v0::message_bus::MessageIdVs};
 use std::time::Duration;
+use tonic::{
+    transport::{Channel, Uri},
+    IntoRequest,
+};
+use utils::DEFAULT_REQ_TIMEOUT;
 
 /// Request specific minimum timeouts
 /// zeroing replicas on create/destroy takes some time (observed up to 7seconds)
@@ -51,17 +56,84 @@ pub fn timeout_grpc(op_id: MessageIdVs, min_timeout: Duration) -> Duration {
 
 /// context to be sent along with each request encapsulating the extra add ons that changes the
 /// behaviour of each request.
+#[derive(Clone)]
 pub struct Context {
     timeout_opts: Option<TimeoutOptions>,
 }
 impl Context {
-    /// generate a new context with the provided timeout options
+    /// Generate a new context with the provided `TimeoutOptions`.
     pub fn new(timeout_opts: impl Into<Option<TimeoutOptions>>) -> Self {
         Self {
             timeout_opts: timeout_opts.into(),
         }
     }
+    /// Get the optional `TimeoutOptions`.
     pub fn timeout_opts(&self) -> Option<TimeoutOptions> {
         self.timeout_opts.clone()
+    }
+
+    /// Get the base timeout if specified, or `DEFAULT_REQ_TIMEOUT`.
+    pub fn base_timeout(&self) -> Duration {
+        self.timeout_opts
+            .as_ref()
+            .map(|o| o.base_timeout())
+            .unwrap_or_else(|| humantime::parse_duration(DEFAULT_REQ_TIMEOUT).unwrap())
+    }
+
+    /// Create a new endpoint that connects to the provided Uri.
+    /// This endpoint has default connect and request timeouts.
+    fn endpoint(&self, uri: Uri) -> tonic::transport::Endpoint {
+        let timeout = self.base_timeout();
+        tonic::transport::Endpoint::from(uri)
+            // we use the same timeout for the connection so we can pass the existing nats tests
+            // todo: use a shorter connect timeout
+            .connect_timeout(timeout)
+            .timeout(timeout)
+            .keep_alive_while_idle(true)
+    }
+}
+
+/// Generic RPC Client.
+#[derive(Clone)]
+pub struct Client<C: Clone> {
+    context: Context,
+    client: C,
+}
+
+impl<C: Clone> Client<C> {
+    /// Creates a generic RPC client based on the provided arguments.
+    /// options: Timeout options which are used for connection and request timeouts.
+    /// make_client: Creates a client of the appropriate type.
+    pub(crate) async fn new<O, M>(uri: Uri, options: O, make_client: M) -> Self
+    where
+        O: Into<Option<TimeoutOptions>>,
+        M: FnOnce(Channel) -> C,
+    {
+        let context = Context::new(options);
+        let endpoint = context.endpoint(uri);
+        let channel = endpoint.connect_lazy().unwrap();
+        let client = make_client(channel);
+        Self { context, client }
+    }
+
+    /// Prepares a new `tonic::Request<T>` for the given request `R: Into<T>`.
+    /// If `context` is specified the timeout of the request will be set to the base_timeout of
+    /// context. Otherwise, `op_id` will be used to select an appropriate timeout.
+    pub(crate) fn request<T, R: Into<T>>(
+        &self,
+        request: R,
+        context: Option<Context>,
+        op_id: MessageIdVs,
+    ) -> tonic::Request<T> {
+        let timeout = context
+            .map(|c| c.base_timeout())
+            .unwrap_or_else(|| timeout_grpc(op_id, self.context.base_timeout()));
+        let mut request = request.into().into_request();
+        request.set_timeout(timeout);
+        request
+    }
+    /// Returns a new client.
+    pub(crate) fn client(&self) -> C {
+        self.client.clone()
     }
 }
