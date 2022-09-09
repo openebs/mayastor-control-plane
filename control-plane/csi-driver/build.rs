cb0198fa0b42576e05ed617287296606e23a45a1
Tiago Castro <tiagolobocastro@gmail.com>
2022-09-09 14:05:29 +0100

    style(csi-driver): refactor csi-driver layout to be idiomatic
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

--- control-plane/csi-driver/build.rs
+++ control-plane/csi-driver/build.rs
@@ -2,6 +2,6 @@ fn main() {
     tonic_build::configure()
         .build_server(true)
         .build_client(true)
-        .compile(&["node/proto/node-service.proto"], &["node/proto"])
+        .compile(&["proto/node-service.proto"], &["proto"])
         .expect("node grpc service protobuf compilation failed");
 }
