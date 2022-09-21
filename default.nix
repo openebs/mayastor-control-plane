a7b5d434a5c087f148c82eb40de6a109f782fde7
Tiago Castro <tiagolobocastro@gmail.com>
2022-09-21 16:08:55 +0100

    feat(nix): allow nix system to be specified
    This may allows us to build for arm using binfmt;
    
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

--- default.nix
+++ default.nix
@@ -1,8 +1,10 @@
-{ allInOne ? true, incremental ? false }:
+{ system ? null, allInOne ? true, incremental ? false }:
 let
   sources = import ./nix/sources.nix;
+  hostSystem = (import sources.nixpkgs { }).hostPlatform.system;
   pkgs = import sources.nixpkgs {
     overlays = [ (_: _: { inherit sources; }) (import ./nix/overlay.nix { inherit allInOne incremental; }) ];
+    system = if system != null then system else hostSystem;
   };
 in
 pkgs
