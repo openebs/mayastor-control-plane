4055a644ae07bada1e88e85a6878b07d4fea952e
Abhinandan Purkait <purkaitabhinandan@gmail.com>
2022-09-22 09:28:01 +0000

    feat(ha-target-switchover): add rest api changes
    Signed-off-by: Abhinandan Purkait <purkaitabhinandan@gmail.com>

--- control-plane/rest/tests/v0_test.rs
+++ control-plane/rest/tests/v0_test.rs
@@ -286,6 +286,7 @@ async fn client_test(cluster: &Cluster, auth: &bool) {
             &volume.state.uuid,
             models::VolumeShareProtocol::Nvmf,
             Some(io_engine1.as_str()),
+            Some(false),
         )
         .await
         .unwrap();
