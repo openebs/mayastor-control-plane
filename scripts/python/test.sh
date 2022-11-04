36f3fbf622e786f83b9b7ed6eaef98ded3c05847
Tiago Castro <tiagolobocastro@gmail.com>
2022-11-04 16:01:36 +0000

    fix(bdd): trap errors correctly
    Make cleanup script executable and add trap before the tests are run otherwise we
    hide execution errors and CI always passes even on failure..
    
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

--- scripts/python/test.sh
+++ scripts/python/test.sh
@@ -13,6 +13,10 @@ set -e
 SCRIPT_DIR="$(dirname "$0")"
 export ROOT_DIR="$SCRIPT_DIR/../.."
 
+cleanup_handler() {
+  "$ROOT_DIR"/scripts/python/test_residue_cleanup.sh
+}
+
 # FAST mode to avoid rebuilding certain dependencies
 if [ -n "$FAST" ]; then
   echo "FAST enabled - will not rebuild the csi&openapi clients nor the deployer. (Make sure they are built already)"
@@ -21,15 +25,11 @@ fi
 # shellcheck source=/dev/null
 . "$ROOT_DIR"/tests/bdd/setup.sh
 
+trap cleanup_handler ERR INT QUIT TERM HUP
+
 # Extra arguments will be provided directly to pytest, otherwise the bdd folder will be tested with default arguments
 if [ $# -eq 0 ]; then
   pytest "$BDD_TEST_DIR"
 else
   pytest "$@"
 fi
-
-cleanup_handler() {
-  "$ROOT_DIR"/scripts/python/test_residue_cleanup.sh
-}
-
-trap cleanup_handler ERR INT QUIT TERM HUP
