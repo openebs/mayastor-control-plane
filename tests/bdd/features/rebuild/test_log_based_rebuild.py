5091c5b2802888654797ab36eb2bb8c3509ee84a
Abhilash Shetty <abhilash.shetty@datacore.com>
2023-04-27 18:48:50 +0000

    test(partial-rebuild): add bdd tests for replica rebuild
    Signed-off-by: Abhilash Shetty <abhilash.shetty@datacore.com>

--- tests/bdd/features/rebuild/test_log_based_rebuild.py
+++ tests/bdd/features/rebuild/test_log_based_rebuild.py
@@ -8,32 +8,30 @@ from pytest_bdd import (
 )
 
 import pytest
-import http
 import os
 import subprocess
 import time
-from urllib.parse import urlparse
 from retrying import retry
 
 from common.deployer import Deployer
 from common.apiclient import ApiClient
 from common.docker import Docker
+from common.nvme import nvme_connect, nvme_disconnect
+from time import sleep
 from common.fio import Fio
 
 from openapi.model.create_pool_body import CreatePoolBody
 from openapi.model.create_volume_body import CreateVolumeBody
 from openapi.model.publish_volume_body import PublishVolumeBody
 from openapi.model.protocol import Protocol
-from openapi.exceptions import ApiException
 from openapi.model.volume_status import VolumeStatus
 from openapi.model.volume_policy import VolumePolicy
 from openapi.model.pool_status import PoolStatus
 from openapi.model.replica_state import ReplicaState
 from openapi.model.child_state import ChildState
+from openapi.model.child_state_reason import ChildStateReason
 
 VOLUME_UUID = "5cd5378e-3f05-47f1-a830-a0f5873a1449"
-VOLUME_SIZE = 10485761
-NUM_VOLUME_REPLICAS = 3
 NODE_1_NAME = "io-engine-1"
 NODE_2_NAME = "io-engine-2"
 NODE_3_NAME = "io-engine-3"
@@ -42,8 +40,15 @@ POOL_1_UUID = "4cc6ee64-7232-497d-a26f-38284a444980"
 POOL_2_UUID = "91a60318-bcfe-4e36-92cb-ddc7abf212ea"
 POOL_3_UUID = "4d471e62-ca17-44d1-a6d3-8820f6156c1a"
 POOL_4_UUID = "d5c5e3de-d77b-11ed-afa1-0242ac120002"
-RECONCILE_PERIOD_SECS = 1
-FIO_RUNTIME = 120
+VOLUME_SIZE = 524288000
+POOL_SIZE = 734003200
+NUM_VOLUME_REPLICAS = 3
+RECONCILE_PERIOD_SECS = "3s"
+FAULTED_CHILD_WAIT = "12s"
+FIO_RUN = 15
+SLEEP_BEFORE_START = 5
+WAIT_FOR_FULL_REBUILD = 16
+REPUBLISH_WAIT = 12
 faulted_child_uri = None
 
 
@@ -52,9 +57,9 @@ def init(disks):
     Deployer.start(
         io_engines="4",
         wait="10s",
-        reconcile_period=f"{RECONCILE_PERIOD_SECS}s",
-        faulted_child_wait_period="15s",
-        cache_period="1s",
+        reconcile_period=RECONCILE_PERIOD_SECS,
+        faulted_child_wait_period=FAULTED_CHILD_WAIT,
+        cache_period="500ms",
     )
 
     assert len(disks) == (NUM_VOLUME_REPLICAS + 1)
@@ -89,7 +94,7 @@ def disks(tmp_files):
         if os.path.exists(disk):
             os.remove(disk)
         with open(disk, "w") as file:
-            file.truncate(100 * 1024 * 1024)
+            file.truncate(POOL_SIZE)
 
     # /tmp is mapped into /host/tmp within the io-engine containers
     yield list(map(lambda file: f"/host{file}", tmp_files))
@@ -117,25 +122,16 @@ def test_faulted_child_is_online_again_within_timedwait_period():
 
 @scenario(
     "log-based-rebuild.feature",
-    "Node goes permanently down while log-based rebuild running",
+    "Node goes permanently down while log based rebuild running",
 )
-def test_node_goes_permanently_down_while_logbased_rebuild_running():
-    """Node goes permanently down while log-based rebuild running."""
+@pytest.mark.skip(reason="blocked due to GTM-684")
+def test_node_goes_permanently_down_while_log_based_rebuild_running():
+    """Node goes permanently down while log based rebuild running."""
 
 
-@scenario(
-    "log-based-rebuild.feature",
-    "Volume target is moved before starting log-based rebuild",
-)
-def test_volume_target_is_moved_before_starting_logbased_rebuild():
-    """Volume target is moved before starting log-based rebuild."""
-
-
-@scenario(
-    "log-based-rebuild.feature", "Volume target is moved during log-based rebuild"
-)
-def test_volume_target_is_moved_during_logbased_rebuild():
-    """Volume target is moved during log-based rebuild."""
+@given("io-engine is installed and running")
+def io_engine_is_installed_and_running():
+    """io-engine is installed and running."""
 
 
 @given("a volume with three replicas, filled with user data")
@@ -156,23 +152,18 @@ def a_volume_with_three_replicas_filled_with_user_data(disks):
     ApiClient.pools_api().put_node_pool(
         NODE_4_NAME, POOL_4_UUID, CreatePoolBody([f"aio://{disks[3]}"])
     )
-
-    # Launch fio in background and let it always run along with the test.
-    """
-    uri = urlparse(volume["state"]["target"]["deviceUri"])
-    fio = Fio("job", "randrw", uri=uri, runtime=FIO_RUNTIME).build_for_userspace()
+    # Launch fio in background and let it run along with the test.
+    uri = volume["state"]["target"]["deviceUri"]
+    disk = nvme_connect(uri)
+    fio = Fio(name="job", rw="randwrite", device=disk, runtime=FIO_RUN)
+    fio = fio.open()
+    yield
     try:
-        subprocess.run(fio, shell=True, check=True)
-    except subprocess.CalledProcessError:
-        remove_stale_files_on_error(uri.hostname, uri.path[1:])
-        assert False, "FIO is not expected to be errored out"
-    """
-
-
-@given("io-engine is installed and running")
-def io_engine_is_installed_and_running():
-    """io-engine is installed and running."""
-    pass
+        code = fio.wait(timeout=FIO_RUN)
+        assert code == 0, "FIO failed, exit code: %d" % code
+    except subprocess.TimeoutExpired:
+        assert False, "FIO timed out"
+    nvme_disconnect(uri)
 
 
 @when("a child becomes faulted")
@@ -194,22 +185,14 @@ def a_child_becomes_faulted():
 @when("a non-local child becomes faulted")
 def a_non_local_child_becomes_faulted():
     """a non-local child becomes faulted."""
-    pass
-
-
-@when(
-    "network connection between nexus and a replica node temporarily goes down and restored"
-)
-def _():
-    """network connection between nexus and a replica node temporarily goes down and restored."""
-    pass
+    a_child_becomes_faulted()
 
 
 @when("the replica is not online again within the timed-wait period")
 def the_replica_is_not_online_again_within_the_timed_wait_period():
     """the replica is not online again within the timed-wait period."""
     # The replica shouldn't be available on this node still.
-    time.sleep(2)
+    time.sleep(WAIT_FOR_FULL_REBUILD)
     replica = ApiClient.replicas_api().get_node_replicas(NODE_2_NAME)
     assert len(replica) == 0, "Replica not expected to be available on " + NODE_2_NAME
 
@@ -217,52 +200,39 @@ def the_replica_is_not_online_again_within_the_timed_wait_period():
 @when("the replica is online again within the timed-wait period")
 def the_replica_is_online_again_within_the_timed_wait_period():
     """the replica is online again within the timed-wait period."""
-    # The control plane should have attempted the online child by now.
-    # Let's wait a few moments.
-    time.sleep(2)
+    # this sleep would ensure some new writes to be in the log
+    sleep(SLEEP_BEFORE_START)
+    Docker.restart_container(NODE_2_NAME)
+    # allow replica to be online after restart
+    check_replica_online()
+
+
+@retry(wait_fixed=500, stop_max_attempt_number=20)
+def check_replica_online():
     replica = ApiClient.replicas_api().get_node_replicas(NODE_2_NAME)
     if len(replica) == 1:
         assert replica[0].state == ReplicaState("Online")
     else:
-        # assert False, "Expected a replica to be available again on " + NODE_2_NAME
-        pass
-
-
-@when("the volume target moves to a different node before the log-based rebuild starts")
-def _():
-    """the volume target moves to a different node before the log-based rebuild starts."""
-    pass
+        assert False, "Expected a replica to be available again on " + NODE_2_NAME
 
 
 @then("a full rebuild starts after some time")
+@retry(wait_fixed=500, stop_max_attempt_number=20)
 def a_full_rebuild_starts_after_some_time():
     """a full rebuild starts after some time."""
     vol = ApiClient.volumes_api().get_volume(VOLUME_UUID)
     target = vol.state.target
     childlist = target["children"]
     assert len(childlist) == NUM_VOLUME_REPLICAS
+    check_child_removal()
     for child in childlist:
-        if child["state"] == ChildState("Faulted") or child["state"] == ChildState(
-            "Degraded"
-        ):
+        if child["state"] == ChildState("Degraded"):
             assert child["uri"] != faulted_child_uri
             assert child["rebuild_progress"] != 0
 
 
-@then("a log-based rebuild is started after reconnection")
-def _():
-    """a log-based rebuild is started after reconnection."""
-    pass
-
-
-@then("a log-based rebuild is started after the io-engine restarts")
-def _():
-    """a log-based rebuild is started after the io-engine restarts."""
-    pass
-
-
 @then("log-based rebuild starts after some time")
-@retry(wait_fixed=1000, stop_max_attempt_number=2)
+@retry(wait_fixed=500, stop_max_attempt_number=20)
 def log_based_rebuild_starts_after_some_time():
     """log-based rebuild starts after some time."""
     # TODO: Check log-based rebuild is running.
@@ -272,51 +242,33 @@ def log_based_rebuild_starts_after_some_time():
     childlist = target["children"]
     assert len(childlist) == NUM_VOLUME_REPLICAS
     for child in childlist:
-        if child["state"] == ChildState("Faulted") or child["state"] == ChildState(
-            "Degraded"
-        ):
-            assert child["uri"] == faulted_child_uri
-            assert child["rebuild_progress"] != 0
+        if child["uri"] == faulted_child_uri:
+            assert child["state"] == ChildState("Degraded")
+            assert child["state_reason"] == ChildStateReason("OutOfSync")
 
 
 @then("the node hosting rebuilding replica crashes permanently")
-def _():
+def the_node_hosting_rebuilding_replica_crashes_permanently():
     """the node hosting rebuilding replica crashes permanently."""
-    pass
+    Docker.kill_container(NODE_2_NAME)
+    check_child_removal()
 
 
-@then("the replica is not online again within the timeout period")
-def _():
-    """the replica is not online again within the timeout period."""
-    pass
+@retry(wait_fixed=500, stop_max_attempt_number=2)
+def check_child_removal():
+    vol = ApiClient.volumes_api().get_volume(VOLUME_UUID)
+    child_list = vol.state.target["children"]
+    child_removed = True
+    for child in child_list:
+        if faulted_child_uri == child["uri"]:
+            child_removed = False
+            break
+    assert (
+        child_removed
+    ), f"child list is {child_list}, faulted child : {faulted_child_uri}"
 
 
-@then("the volume target moves to a different node before rebuild completion")
-def _():
-    """the volume target moves to a different node before rebuild completion."""
-    pass
-
-
-@retry(wait_fixed=1000, stop_max_attempt_number=10)
+@retry(wait_fixed=500, stop_max_attempt_number=20)
 def wait_for_degraded_volume():
     volume = ApiClient.volumes_api().get_volume(VOLUME_UUID)
     assert volume.state.status == VolumeStatus("Degraded")
-
-
-@retry(wait_fixed=1000, stop_max_attempt_number=10)
-def wait_for_pool_online(pool_uuid):
-    pool = ApiClient.pools_api().get_pool(pool_uuid)
-    assert pool.state.status == PoolStatus("Online")
-
-
-def remove_stale_files_on_error(ip, nqn):
-    filename = (
-        "{}/'trtype=tcp adrfam=IPv4 traddr={} trsvcid=8420 subnqn={} ns=1'".format(
-            os.environ["ROOT_DIR"], ip, nqn
-        )
-    )
-    command = "rm -f {}".format(filename)
-    try:
-        subprocess.run(command, shell=True, check=True)
-    except subprocess.CalledProcessError:
-        assert False, "Could not clean up the stale files, needs manual cleanup"
