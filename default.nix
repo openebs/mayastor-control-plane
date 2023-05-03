0a230a15395406942c24a7566bcd26282020a8a2
Tiago Castro <tiagolobocastro@gmail.com>
2023-05-04 00:19:38 +0100

    build(images): add tag argument
    Allows one to explicitly build the binaries with a given tag, as opposed to the alias-tag
    which simply adds a docker image tag alias.
    
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

--- default.nix
+++ default.nix
@@ -1,9 +1,9 @@
-{ system ? null, allInOne ? true, incremental ? false, img_tag ? "" }:
+{ system ? null, allInOne ? true, incremental ? false, img_tag ? "", tag ? "" }:
 let
   sources = import ./nix/sources.nix;
   hostSystem = (import sources.nixpkgs { }).hostPlatform.system;
   pkgs = import sources.nixpkgs {
-    overlays = [ (_: _: { inherit sources; }) (import ./nix/overlay.nix { inherit allInOne incremental img_tag; }) ];
+    overlays = [ (_: _: { inherit sources; }) (import ./nix/overlay.nix { inherit allInOne incremental img_tag tag; }) ];
     system = if system != null then system else hostSystem;
   };
 in
--- scripts/release.sh
+++ scripts/release.sh
@@ -44,6 +44,7 @@ Options:
   --skip-publish             Don't publish built images.
   --image           <image>  Specify what image to build.
   --alias-tag       <tag>    Explicit alias for short commit hash tag.
+  --tag             <tag>    Explicit tag (overrides the git tag).
   --incremental              Builds components in two stages allowing for faster rebuilds during development.
   --build-bins               Builds all the static binaries.
   --build-bin                Specify which binary to build.
@@ -120,6 +121,14 @@ while [ "$#" -gt 0 ]; do
       ALIAS=$1
       shift
       ;;
+    --tag)
+      shift
+      if [ "$TAG" != "" ]; then
+        echo "Overriding $TAG with $1"
+      fi
+      TAG=$1
+      shift
+      ;;
     --image)
       shift
       IMAGES="$IMAGES $1"
@@ -191,15 +200,21 @@ fi
 alias_tag=
 if [ -n "$ALIAS" ]; then
   alias_tag=$ALIAS
+  # when alias is created from branch-name we want to keep the hash and have it pushed to CI because
+  # the alias will change daily.
+  OVERRIDE_COMMIT_HASH="true"
 elif [ "$BRANCH" == "develop" ]; then
   alias_tag="$BRANCH"
 elif [ "${BRANCH#release-}" != "${BRANCH}" ]; then
   alias_tag="${BRANCH}"
 fi
 
-if [ -z "$CI" ] && [ -z "$TAG" ] && [ -n "$alias_tag" ]; then
-  OVERRIDE_COMMIT_HASH="true"
+if [ -n "$TAG" ] && [ "$TAG" != "$(get_tag)" ]; then
+  # Set the TAG which basically allows building the binaries as if it were a git tag
+  NIX_TAG_ARGS="--argstr tag $TAG"
+  NIX_BUILD="$NIX_BUILD $NIX_TAG_ARGS"
 fi
+
 TAG=${TAG:-$HASH}
 if [ -n "$OVERRIDE_COMMIT_HASH" ]; then
   # Set the TAG to the alias and remove the alias
@@ -239,7 +254,7 @@ done
 if [ -n "$UPLOAD" ] && [ -z "$SKIP_PUBLISH" ]; then
   # Upload them
   for img in $UPLOAD; do
-    if [ -z "$REGISTRY" ] && [ -z "$OVERRIDE_COMMIT_HASH" ] && dockerhub_tag_exists $image $TAG; then
+    if [ -z "$REGISTRY" ] && [ -n "$CI" ] && dockerhub_tag_exists $image $TAG; then
       echo "Skipping $image:$TAG that already exists"
       continue
     fi
