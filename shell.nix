7220f7b0bc8922369012b1194e1bf5316215ab0e
Tiago Castro <tiagolobocastro@gmail.com>
2023-05-09 15:49:51 +0100

    chore: update nixpkgs to 22.11
    This brings a newer nvme-cli and thus we don't need to make it special.
    
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

--- shell.nix
+++ shell.nix
@@ -4,7 +4,6 @@ let
   pkgs = import sources.nixpkgs {
     overlays = [ (_: _: { inherit sources; }) (import ./nix/overlay.nix { }) ];
   };
-  pkgs_22 = import sources.nixpkgs-22_11 { };
 in
 with pkgs;
 let
@@ -44,7 +43,7 @@ mkShell {
   ++ pkgs.lib.optionals (system != "aarch64-darwin") [
     e2fsprogs
     libxfs
-    pkgs_22.nvme-cli
+    nvme-cli
     # python3.9-pyopenssl-22.0.0 marked as broken but fixed on master..
     pytest_inputs
     tini
