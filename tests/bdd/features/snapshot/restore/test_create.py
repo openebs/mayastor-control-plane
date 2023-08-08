ab06fb43fef07aee35eca39813872a5b95516f89
Tiago Castro <tiagolobocastro@gmail.com>
2023-08-08 23:09:14 +0100

    test(snapshot/restore): expand create and destroy bdd
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

--- tests/bdd/features/snapshot/restore/test_create.py
+++ tests/bdd/features/snapshot/restore/test_create.py
@@ -7,6 +7,7 @@ import uuid
 import openapi
 from common.deployer import Deployer
 from common.apiclient import ApiClient
+from common.operations import Volume, Snapshot
 
 from openapi.model.create_pool_body import CreatePoolBody
 from openapi.model.create_volume_body import CreateVolumeBody
@@ -32,7 +33,7 @@ def disks():
 
 @pytest.fixture(scope="module")
 def deployer_cluster(disks):
-    Deployer.start(1, cache_period="100ms", reconcile_period="150ms")
+    Deployer.start(1, fio_spdk=True, cache_period="100ms", reconcile_period="150ms")
     ApiClient.pools_api().put_node_pool(
         Deployer.node_name(0), "pool", CreatePoolBody([disks[0]])
     )
@@ -58,6 +59,11 @@ def test_create_multiple_new_volumes_as_snapshot_restores_for_a_valid_snapshot()
 @scenario("create.feature", "Create a chain of restored volumes")
 def test_create_a_chain_of_restored_volumes():
     """Create a chain of restored volumes."""
+
+
+@scenario("create.feature", "Snapshotting a restored volume")
+def test_snapshotting_a_restored_volume():
+    """Snapshotting a restored volume."""
 
 
 @given("a deployer cluster")
@@ -151,7 +157,7 @@ def a_new_replica_will_be_created_for_the_new_volume(volume_uuids, new_volume):
     assert new_volume.spec.status == SpecStatus("Created")
 
 
-@then("all requests should succeed")
+@then("all requests should succeed", target_fixture="created_restores")
 def all_requests_should_succeed(snaprestore_attempts):
     """all requests should succeed."""
     assert len(snaprestore_attempts["failed"]) == 0
@@ -162,6 +168,7 @@ def all_requests_should_succeed(snaprestore_attempts):
         )
     )
     assert len(created) == 4, f"Only created these: {created}"
+    yield created
 
 
 @then("the replica's capacity will be same as the snapshot")
@@ -172,7 +179,7 @@ def the_replicas_capacity_will_be_same_as_the_snapshot(new_volume):
     assert replicas[0].usage.capacity == new_volume.spec.size
     assert replicas[0].usage.allocated == 0
     # Seems allocated_snapshots also reports snapshots usage for a restore !?
-    assert replicas[0].usage.allocated_snapshots == new_volume.spec.size
+    assert replicas[0].usage.allocated_snapshots == 0  # new_volume.spec.size
 
 
 @then(
@@ -199,8 +206,92 @@ def we_create_volume_restore_index_with_the_previous_snapshot_as_its_source(
         snapshot_uuids[index - 1], volume_uuids[index], body
     )
     ApiClient.volumes_api().del_volume(volume_uuids[index])
-    if index == 1:
-        volumes = ApiClient.volumes_api().get_volumes()
-        assert len(volumes.entries) == 1
-        snapshots = ApiClient.snapshots_api().get_volumes_snapshots()
-        assert len(snapshots.entries) == 1
+    volumes = ApiClient.volumes_api().get_volumes()
+    assert len(volumes.entries) == index
+    snapshots = ApiClient.snapshots_api().get_volumes_snapshots()
+    assert len(snapshots.entries) == index
+
+
+@then("the restored volume own allocated_snapshot size should be 0")
+def the_restored_volume_own_allocated_snapshot_size_should_be_0(restored_volume):
+    """the restored volume own allocated_snapshot size should be 0."""
+    assert restored_volume.state.usage.allocated_replica == 0
+    assert restored_volume.state.usage.allocated_snapshots == 0
+    assert restored_volume.state.usage.total_allocated == 0
+
+
+@then("the restored volume's own allocated_snapshot size should be 0")
+def the_restored_volumes_own_allocated_snapshot_size_should_be_0(created_restores):
+    """the restored volume's own allocated_snapshot size should be 0."""
+    for restore in created_restores:
+        assert restore.state.usage.allocated_replica == 0
+        assert restore.state.usage.allocated_snapshots == 0
+        assert restore.state.usage.total_allocated == 0
+
+
+@given(
+    "we create a new volume with the snapshot as its source",
+    target_fixture="restored_volume",
+)
+def we_create_a_new_volume_with_the_snapshot_as_its_source(
+    volume_uuids, snapshot_uuids
+):
+    """we create a new volume with the snapshot as its source."""
+    body = CreateVolumeBody(
+        VolumePolicy(True),
+        replicas=1,
+        size=20 * 1024 * 1024,
+        thin=True,
+    )
+    volume = ApiClient.volumes_api().put_snapshot_volume(
+        snapshot_uuids[0], volume_uuids[1], body
+    )
+    yield volume
+    Volume.cleanup(volume)
+
+
+@then("we allocate 4MiB of data of the restored volume")
+@when("we allocate 4MiB of data of the restored volume")
+def we_allocate_4mib_of_data_of_the_restored_volume(restored_volume):
+    """we allocate 4MiB of data of the restored volume."""
+    Volume.fio(restored_volume, offset="7M", size="4096B")
+    Volume.update(restored_volume, cached=False)
+
+
+@then("the restored volume allocation size should be 4MiB")
+def the_restored_volume_allocation_size_should_be_4mib(restored_volume):
+    """the restored volume allocation size should be 4MiB."""
+    volume = Volume.update(restored_volume)
+    assert volume.state.usage.allocated_replica == 4 * 1024 * 1024
+    assert volume.state.usage.allocated_snapshots == 0
+    assert volume.state.usage.total_allocated == 4 * 1024 * 1024
+
+
+@then("the restored volume total allocation size should be 4MiB")
+def the_restored_volume_total_allocation_size_should_be_4mib(restored_volume):
+    """the restored volume total allocation size should be 4MiB."""
+    volume = Volume.update(restored_volume)
+    assert volume.state.usage.total_allocated == 4 * 1024 * 1024
+
+
+@then("the restored volume total allocation size should be 8MiB")
+def the_restored_volume_total_allocation_size_should_be_8mib(restored_volume):
+    """the restored volume total allocation size should be 8MiB."""
+    volume = Volume.update(restored_volume)
+    assert volume.state.usage.total_allocated == 8 * 1024 * 1024
+
+
+@then("the snapshot allocated size should be 4MiB")
+def the_snapshot_allocated_size_should_be_4mib(restore_snapshot):
+    """the snapshot allocated size should be 4MiB."""
+    assert restore_snapshot.state.allocated_size == 4 * 1024 * 1024
+
+
+@then("we create a snapshot of the restored volume", target_fixture="restore_snapshot")
+def we_create_a_snapshot_of_the_restored_volume(snapshot_uuids, volume_uuids):
+    """we create a snapshot of the restored volume."""
+    snapshot = ApiClient.snapshots_api().put_volume_snapshot(
+        volume_uuids[1], snapshot_uuids[1]
+    )
+    yield snapshot
+    Snapshot.cleanup(snapshot)
--- tests/bdd/features/snapshot/restore/test_delete.py
+++ tests/bdd/features/snapshot/restore/test_delete.py
@@ -7,7 +7,7 @@ import uuid
 from common.deployer import Deployer
 from common.apiclient import ApiClient
 from common.docker import Docker
-from common.operations import Volume, Snapshot, wait_node_online
+from common.operations import Volume, Snapshot, wait_node_online, Cluster
 
 from openapi.model.create_pool_body import CreatePoolBody
 from openapi.model.create_volume_body import CreateVolumeBody
@@ -44,13 +44,21 @@ def deployer_cluster(disks):
 
 
 @scenario("delete.feature", "Deleting in creation order")
+@pytest.mark.skip("This test is awaiting changes from data-plane & control-plane")
 def test_deleting_in_creation_order():
     """Deleting in creation order."""
 
 
 @scenario("delete.feature", "Deleting in reverse creation order")
+@pytest.mark.skip("This test is awaiting changes from data-plane & control-plane")
 def test_deleting_in_reverse_creation_order():
     """Deleting in reverse creation order."""
+
+
+@scenario("delete.feature", "Delete a chain of restored volumes")
+@pytest.mark.skip("This test is awaiting changes from data-plane & control-plane")
+def test_delete_a_chain_of_restored_volumes():
+    """Delete a chain of restored volumes."""
 
 
 @given("a deployer cluster")
@@ -59,9 +67,7 @@ def a_deployer_cluster(deployer_cluster):
 
 
 @given("a single replica volume from the snapshot", target_fixture="restored_volume")
-def a_single_replica_volume_from_the_snapshot(
-    original_snapshot, volume_uuids, snapshot_uuids
-):
+def a_single_replica_volume_from_the_snapshot(original_snapshot, volume_uuids):
     """a single replica volume from the snapshot."""
     volume = ApiClient.volumes_api().put_snapshot_volume(
         original_snapshot.definition.spec.uuid,
@@ -73,8 +79,6 @@ def a_single_replica_volume_from_the_snapshot(
             thin=True,
         ),
     )
-    # allocate 1 cluster worth of data
-    volume = Volume.fio(volume, offset="7M", size="4096B")
     yield volume
     Volume.cleanup(volume)
 
@@ -85,17 +89,13 @@ def a_snapshot(original_volume, snapshot_uuids):
     snapshot = ApiClient.snapshots_api().put_volume_snapshot(
         original_volume.spec.uuid, snapshot_uuids[0]
     )
-    # Why 3M you ask? Well because we know that we have 5M of unused partition at the start of the replica
-    # so if we write before, we might span 2 clusters instead of 1, which could be confusing
-    volume = Volume.fio(original_volume, offset="3M", size="4096B")
-    snapshot = Snapshot.update(snapshot, original_volume, cached=False)
     yield snapshot
     Snapshot.cleanup(snapshot)
 
 
-@given("a single replica volume", target_fixture="original_volume")
-def a_single_replica_volume(volume_uuids):
-    """a single replica volume."""
+@given("a single replica volume with 8MiB allocated", target_fixture="original_volume")
+def a_single_replica_volume_with_8mib_allocated(volume_uuids):
+    """a single replica volume with 8MiB allocated."""
     volume = ApiClient.volumes_api().put_volume(
         volume_uuids[0],
         CreateVolumeBody(
@@ -105,8 +105,19 @@ def a_single_replica_volume(volume_uuids):
             thin=True,
         ),
     )
+    # 8MiB get allocated on volume creation
+    assert volume.state.usage.allocated == 8 * 1024 * 1024
     yield volume
     Volume.cleanup(volume)
+
+
+@given("we allocate 4MiB of the original volume")
+def we_allocate_4mib_of_the_original_volume_1(original_volume):
+    """we allocate 4MiB of the original volume."""
+    # Why 3M you ask? Well because we know that we have 5M of unused partition at the start of the replica
+    # so if we write before, we might span 2 clusters instead of 1, which could be confusing
+    volume = Volume.fio(original_volume, offset="3M", size="4096B")
+    Volume.update(volume, cached=False)
 
 
 @when("we delete the original volume")
@@ -175,6 +186,298 @@ def the_snapshot_should_still_exist(original_volume, original_snapshot):
 
 
 @then("we delete the snapshot")
+@when("we delete the snapshot")
 def we_delete_the_snapshot(original_snapshot):
     """we delete the snapshot."""
     ApiClient.snapshots_api().del_snapshot(original_snapshot.definition.spec.uuid)
+
+
+@given("the restored volume 1 allocated_replica size should be 4MiB")
+def the_restored_volume_1_allocated_replica_size_should_be_4mib(restored_volume):
+    """the restored volume 1 allocated_replica size should be 4MiB."""
+    volume = Volume.update(restored_volume)
+    assert volume.state.usage.allocated_replica == 4 * 1024 * 1024
+
+
+@given("the restored volume 1 allocated_snapshot size should be zero")
+def the_restored_volume_1_allocated_snapshot_size_should_be_zero(restored_volume):
+    """the restored volume 1 allocated_snapshot size should be zero."""
+    volume = Volume.update(restored_volume)
+    assert volume.state.usage.allocated_snapshots == 0
+
+
+@given("we allocate 4MiB of the restored volume 1")
+@then("we allocate 4MiB of the restored volume 1")
+def we_allocate_4mib_of_the_restored_volume_1(restored_volume):
+    """we allocate 4MiB of the restored volume 1."""
+    # Why 3M you ask? Well because we know that we have 5M of unused partition at the start of the replica
+    # so if we write before, we might span 2 clusters instead of 1, which could be confusing
+    volume = Volume.fio(restored_volume, offset="3M", size="4096B")
+    Volume.update(volume, cached=False)
+
+
+@given("the restored volume 1 allocation size should be 4MiB")
+def the_restored_volume_1_allocation_size_should_be_4mib(restored_volume):
+    """the restored volume 1 allocation size should be 4MiB."""
+    volume = Volume.update(restored_volume)
+    assert volume.state.usage.allocated == 4 * 1024 * 1024
+
+
+@given("the restored volume 1 allocated_replica size should be zero")
+@then("the restored volume 1 allocated_replica size should be zero")
+def the_restored_volume_1_allocated_replica_size_should_be_zero(restored_volume):
+    """the restored volume 1 allocated_replica size should be zero."""
+    volume = Volume.update(restored_volume)
+    assert volume.state.usage.allocated_replica == 0
+
+
+@then("the restored volume 1 allocated_snapshot size should be 4MiB")
+def the_restored_volume_1_allocated_snapshot_size_should_be_4mib(restored_volume):
+    """the restored volume 1 allocated_snapshot size should be 4MiB."""
+    volume = Volume.update(restored_volume)
+    assert volume.state.usage.allocated_snapshots == 4 * 1024 * 1024
+
+
+@given("the restored volume 1 allocation size should be 12MiB")
+def the_restored_volume_1_allocation_size_should_be_12mib(restored_volume):
+    """the restored volume 1 allocation size should be 12MiB."""
+    volume = Volume.update(restored_volume)
+    assert volume.state.usage.allocated == 12 * 1024 * 1024
+
+
+@then("the restored volume 1 snapshot 1 allocation size should be 4MiB")
+def the_restored_volume_1_snapshot_1_allocation_size_should_be_4mib(
+    restored_1_snapshot_1,
+):
+    """the restored volume 1 snapshot 1 allocation size should be 4MiB."""
+    snapshot = Snapshot.update(restored_1_snapshot_1)
+    assert snapshot.state.allocated_size == 4 * 1024 * 1024
+
+
+@then("the restored volume 1 snapshot 1 allocation size should be 12MiB")
+@when("the restored volume 1 snapshot 1 allocation size should be 12MiB")
+def the_restored_volume_1_snapshot_1_allocation_size_should_be_12mib(
+    restored_1_snapshot_1,
+):
+    """the restored volume 1 snapshot 1 allocation size should be 12MiB."""
+    Docker.restart_container(NODE)
+    wait_node_online(NODE)
+    Cluster.wait_cache_update()
+    snapshot = Snapshot.update(restored_1_snapshot_1)
+    assert snapshot.state.allocated_size == 12 * 1024 * 1024
+
+
+@then("the restored volume 1 snapshot 2 allocation size should be 4MiB")
+@when("the restored volume 1 snapshot 2 allocation size should be 4MiB")
+def the_restored_volume_1_snapshot_2_allocation_size_should_be_4mib(
+    restored_1_snapshot_2,
+):
+    """the restored volume 1 snapshot 2 allocation size should be 4MiB."""
+    snapshot = Snapshot.update(restored_1_snapshot_2)
+    assert snapshot.state.allocated_size == 4 * 1024 * 1024
+
+
+@then("the restored volume 1 snapshot 2 total allocation size should be 8MiB")
+@when("the restored volume 1 snapshot 2 total allocation size should be 8MiB")
+def the_restored_volume_1_snapshot_2_total_allocation_size_should_be_8mib(
+    restored_1_snapshot_2,
+):
+    """the restored volume 1 snapshot 2 total allocation size should be 8MiB."""
+    snapshot = Snapshot.update(restored_1_snapshot_2)
+    assert snapshot.state.allocated_size == 4 * 1024 * 1024
+    # todo: missing this on snapshot state!
+    assert snapshot.definition.metadata.total_allocated_size == 8 * 1024 * 1024
+
+
+@given("we create restored volume 1 snapshot 1", target_fixture="restored_1_snapshot_1")
+def we_create_restored_volume_1_snapshot_1(snapshot_uuids, restored_volume):
+    """we create restored volume 1 snapshot 1."""
+    snapshot = ApiClient.snapshots_api().put_volume_snapshot(
+        restored_volume.spec.uuid, snapshot_uuids[1]
+    )
+    yield snapshot
+    Snapshot.cleanup(snapshot)
+
+
+@then("we create restored volume 1 snapshot 2", target_fixture="restored_1_snapshot_2")
+def we_create_restored_volume_1_snapshot_2(snapshot_uuids, restored_volume):
+    """we create restored volume 1 snapshot 2."""
+    snapshot = ApiClient.snapshots_api().put_volume_snapshot(
+        restored_volume.spec.uuid, snapshot_uuids[2]
+    )
+    yield snapshot
+    Snapshot.cleanup(snapshot)
+
+
+@then(
+    "we restore volume 1 snapshot 2 into restored volume 2",
+    target_fixture="volume_2_from_snapshot_2",
+)
+def we_restore_volume_1_snapshot_2_into_restored_volume_2(
+    restored_1_snapshot_2, volume_uuids
+):
+    """we restore volume 1 snapshot 2 into restored volume 2."""
+    volume = ApiClient.volumes_api().put_snapshot_volume(
+        restored_1_snapshot_2.definition.spec.uuid,
+        volume_uuids[2],
+        CreateVolumeBody(
+            VolumePolicy(True),
+            replicas=1,
+            size=restored_1_snapshot_2.definition.metadata.size,
+            thin=True,
+        ),
+    )
+    yield volume
+
+
+@then("we allocate 4MiB of the restored volume 2")
+def we_allocate_4mib_of_the_restored_volume_2(volume_2_from_snapshot_2):
+    """we allocate 4MiB of the restored volume 2."""
+    volume = Volume.fio(volume_2_from_snapshot_2, offset="3M", size="4096B")
+    Volume.update(volume, cached=False)
+
+
+@then("the restored volume 2 allocation size should be 4MiB")
+def the_restored_volume_2_allocation_size_should_be_4mib(volume_2_from_snapshot_2):
+    """the restored volume 2 allocation size should be 4MiB."""
+    volume = Volume.update(volume_2_from_snapshot_2, cached=False)
+    assert volume.state.usage.allocated_replica == 4 * 1024 * 1024
+    assert volume.state.usage.allocated_snapshots == 4 * 1024 * 1024
+    assert volume.state.usage.allocated == 8 * 1024 * 1024
+
+
+@then(
+    "the pool space usage should reflect the original snapshot, restored volume 1, snapshot 1, 2, and restored volume 2 (20MiB)"
+)
+def the_pool_space_usage_should_reflect_the_original_snapshot_restored_volume_1_snapshot_1_2_and_restored_volume_2_20mib(
+    restored_volume,
+    volume_2_from_snapshot_2,
+    original_snapshot,
+    restored_1_snapshot_1,
+    restored_1_snapshot_2,
+):
+    """the pool space usage should reflect the original snapshot, restored volume 1, snapshot 1, 2, and restored volume 2 (20MiB)."""
+    pool = ApiClient.pools_api().get_pool(POOL)
+    original_snapshot = Snapshot.update(original_snapshot)
+    restored_volume = Volume.update(restored_volume)
+    restored_1_snapshot_1 = Snapshot.update(restored_1_snapshot_1)
+    restored_1_snapshot_2 = Snapshot.update(restored_1_snapshot_2)
+    volume_2_from_snapshot_2 = Volume.update(volume_2_from_snapshot_2)
+
+    assert (
+        pool.state.used
+        == original_snapshot.state.allocated_size
+        + restored_volume.state.usage.allocated_replica
+        + restored_1_snapshot_1.state.allocated_size
+        + restored_1_snapshot_2.state.allocated_size
+        + volume_2_from_snapshot_2.state.usage.allocated_replica
+    )
+    assert pool.state.used == 20 * 1024 * 1024
+
+
+@then(
+    "the pool space usage should reflect the restored volume 1, snapshot 1, 2, restored volume 2, and deleted snapshot (20MiB)"
+)
+def the_pool_space_usage_should_reflect_the_restored_volume_1_snapshot_1_2_restored_volume_2_and_deleted_snapshot_20mib(
+    restored_volume,
+    volume_2_from_snapshot_2,
+    original_snapshot,
+    restored_1_snapshot_1,
+    restored_1_snapshot_2,
+):
+    """the pool space usage should reflect the restored volume 1, snapshot 1, 2, restored volume 2, and deleted snapshot (20MiB)."""
+    pool = ApiClient.pools_api().get_pool(POOL)
+    restored_volume = Volume.update(restored_volume)
+    restored_1_snapshot_1 = Snapshot.update(restored_1_snapshot_1)
+    restored_1_snapshot_2 = Snapshot.update(restored_1_snapshot_2)
+    volume_2_from_snapshot_2 = Volume.update(volume_2_from_snapshot_2)
+
+    assert (
+        pool.state.used
+        == original_snapshot.state.allocated_size
+        + restored_volume.state.usage.allocated_replica
+        + restored_1_snapshot_1.state.allocated_size
+        + restored_1_snapshot_2.state.allocated_size
+        + volume_2_from_snapshot_2.state.usage.allocated_replica
+    )
+    assert pool.state.used == 20 * 1024 * 1024
+
+
+@then(
+    "the pool space usage should reflect the snapshot 1, 2, restored volume 2, and deleted snapshot and deleted restored volume 1 (20MiB)"
+)
+def the_pool_space_usage_should_reflect_the_snapshot_1_2_restored_volume_2_and_deleted_snapshot_and_deleted_restored_volume_1_20mib(
+    volume_2_from_snapshot_2,
+    restored_1_snapshot_1,
+    restored_1_snapshot_2,
+):
+    """the pool space usage should reflect the snapshot 1, 2, restored volume 2, and deleted snapshot and deleted restored volume 1 (20MiB)."""
+    pool = ApiClient.pools_api().get_pool(POOL)
+    restored_1_snapshot_1 = Snapshot.update(restored_1_snapshot_1)
+    restored_1_snapshot_2 = Snapshot.update(restored_1_snapshot_2)
+    volume_2_from_snapshot_2 = Volume.update(volume_2_from_snapshot_2)
+
+    assert (
+        pool.state.used
+        == restored_1_snapshot_1.state.allocated_size
+        + restored_1_snapshot_2.state.allocated_size
+        + volume_2_from_snapshot_2.state.usage.allocated_replica
+    )
+    assert pool.state.used == 20 * 1024 * 1024
+
+
+@then(
+    "the pool space usage should reflect the snapshot 2, restored volume 2, and deleted snapshot and deleted restored volume 1 (16MiB)"
+)
+def the_pool_space_usage_should_reflect_the_snapshot_2_restored_volume_2_and_deleted_snapshot_and_deleted_restored_volume_1_16mib(
+    volume_2_from_snapshot_2,
+    restored_1_snapshot_2,
+):
+    """the pool space usage should reflect the snapshot 2, restored volume 2, and deleted snapshot and deleted restored volume 1 (16MiB)."""
+    # Bug, dataplane caches allocated, requires a restart until fixed
+    Docker.restart_container(NODE)
+    wait_node_online(NODE)
+    Cluster.wait_cache_update()
+
+    pool = ApiClient.pools_api().get_pool(POOL)
+    restored_1_snapshot_2 = Snapshot.update(restored_1_snapshot_2)
+    volume_2_from_snapshot_2 = Volume.update(volume_2_from_snapshot_2)
+
+    assert (
+        pool.state.used
+        == restored_1_snapshot_2.state.allocated_size
+        + volume_2_from_snapshot_2.state.usage.allocated_replica
+    )
+    assert pool.state.used == 16 * 1024 * 1024
+
+
+@then(
+    "the pool space usage should reflect the restored volume 2, and deleted snapshot 1,2 and deleted restored volume 1 (16MiB)"
+)
+def the_pool_space_usage_should_reflect_the_restored_volume_2_and_deleted_snapshot_12_and_deleted_restored_volume_1_16mib(
+    volume_2_from_snapshot_2,
+):
+    """the pool space usage should reflect the restored volume 2, and deleted snapshot 1,2 and deleted restored volume 1 (16MiB)."""
+    pool = ApiClient.pools_api().get_pool(POOL)
+    volume_2_from_snapshot_2 = Volume.update(volume_2_from_snapshot_2)
+
+    assert pool.state.used == volume_2_from_snapshot_2.state.usage.allocated
+    assert pool.state.used == 16 * 1024 * 1024
+
+
+@when("we delete the restored volume 1 snapshot 1")
+def we_delete_the_restored_volume_1_snapshot_1(restored_1_snapshot_1):
+    """we delete the restored volume 1 snapshot 1.""" ""
+    ApiClient.snapshots_api().del_snapshot(restored_1_snapshot_1.definition.spec.uuid)
+
+
+@when("we delete the restored volume 1 snapshot 2")
+def we_delete_the_restored_volume_1_snapshot_2(restored_1_snapshot_2):
+    """we delete the restored volume 1 snapshot 2."""
+    ApiClient.snapshots_api().del_snapshot(restored_1_snapshot_2.definition.spec.uuid)
+
+
+@when("we delete the restored volume 2")
+def we_delete_the_restored_volume_2(volume_2_from_snapshot_2):
+    """we delete the restored volume 2."""
+    ApiClient.volumes_api().del_volume(volume_2_from_snapshot_2.spec.uuid)
