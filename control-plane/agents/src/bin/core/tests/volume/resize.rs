8dea6d272ea0dd30a33eb7adf3866029a6d9b7f4
Diwakar Sharma <diwakar.sharma@datacore.com>
2024-02-05 11:38:22 +0000

    test(volume/resize): add test for concurrent cluster capacity limit checks
    Signed-off-by: Diwakar Sharma <diwakar.sharma@datacore.com>

--- control-plane/agents/src/bin/core/tests/volume/resize.rs
+++ control-plane/agents/src/bin/core/tests/volume/resize.rs
@@ -5,11 +5,14 @@ use grpc::operations::{
 };
 use std::time::Duration;
 use stor_port::{
-    transport_api::ReplyErrorKind,
+    transport_api::{ReplyError, ReplyErrorKind},
     types::v0::transport::{
-        CreateVolume, DestroyVolume, Filter, PublishVolume, ResizeVolume, VolumeShareProtocol,
+        CreateVolume, DestroyVolume, Filter, PublishVolume, ResizeVolume, Volume, VolumeId,
+        VolumeShareProtocol,
     },
 };
+
+use uuid::Uuid;
 
 const SIZE: u64 = 50 * 1024 * 1024; // 50MiB
 const EXPANDED_SIZE: u64 = 2 * SIZE; // 100MiB
@@ -233,6 +236,28 @@ async fn resize_on_no_capacity_pool() {
     let vol_obj = &v_arr.entries[0];
     // Size shouldn't have changed.
     assert!(vol_obj.spec().size == volume.spec().size);
+
+    // try a resize again, this time setting cluster capacity limit.
+    let _ = vol_cli
+        .resize(
+            &ResizeVolume {
+                uuid: volume.uuid().clone(),
+                requested_size: EXPANDED_SIZE,
+                cluster_capacity_limit: Some(EXPANDED_SIZE + CAPACITY_LIMIT_DIFF),
+            },
+            None,
+        )
+        .await
+        .expect_err("Expected error due to insufficient pool capacity");
+
+    let v_arr = vol_cli
+        .get(Filter::Volume(volume.spec().uuid), false, None, None)
+        .await
+        .unwrap();
+    let vol_obj = &v_arr.entries[0];
+    // Size shouldn't have changed.
+    assert!(vol_obj.spec().size == volume.spec().size);
+
     // TODO: Add reclaim monitor validations for replicas that got resized as part
     // of this failed volume resize.
 }
@@ -264,6 +289,79 @@ async fn resize_with_cluster_capacity_limit() {
 
     // resize within the capacity limit
     grpc_resize_volume_with_limit(&vol_cli, Some(EXPANDED_SIZE + CAPACITY_LIMIT_DIFF), None).await;
+    // resize a new volume, but reduce the limit set previously. The limit balance
+    // calculations are expected to work based on reduced limit value now.
+    grpc_resize_volume_with_limit(
+        &vol_cli,
+        Some(EXPANDED_SIZE + CAPACITY_LIMIT_DIFF / 2),
+        None,
+    )
+    .await;
+}
+
+// Take 600MiB pool. Create five volumes of 50MiB each, totalling a usage of 250MiB.
+// Set cluster capacity limit to 400MiB and expand all five volumes to 100MiB.
+// Since remaining limit is 150MiB, three volumes should successfully expand and
+// two must fail to expand.
+#[tokio::test]
+async fn resize_with_cluster_capacity_limit_concurrent() {
+    let cluster = ClusterBuilder::builder()
+        .with_rest(false)
+        .with_agents(vec!["core"])
+        .with_io_engines(2)
+        .with_pool(0, "malloc:///p1?size_mb=600")
+        .with_cache_period("1s")
+        .with_reconcile_period(Duration::from_secs(1), Duration::from_secs(1))
+        .with_options(|o| o.with_isolated_io_engine(true))
+        .build()
+        .await
+        .unwrap();
+
+    let num_volumes = 5;
+    let mut success = 0;
+    let mut failure = 0;
+    let vol_cli = cluster.grpc_client().volume();
+    let volume_ids = create_volumes(&vol_cli, 5).await;
+    let mut results = Vec::with_capacity(num_volumes);
+
+    // Create a channel to collect results from the concurrent tasks
+    let (tx, mut rx) = tokio::sync::mpsc::channel::<Result<Volume, ReplyError>>(num_volumes);
+    // For each task
+    let refc_vol_cli = std::sync::Arc::new(vol_cli);
+
+    for volume_id in volume_ids {
+        let refp_vol_cli = std::sync::Arc::clone(&refc_vol_cli);
+        let tx = tx.clone();
+        tokio::spawn(async move {
+            let hdl = refp_vol_cli
+                .resize(
+                    &ResizeVolume {
+                        uuid: volume_id.try_into().unwrap(),
+                        requested_size: EXPANDED_SIZE,
+                        cluster_capacity_limit: Some(4 * EXPANDED_SIZE),
+                    },
+                    None,
+                )
+                .await;
+
+            // Send the result to the channel
+            tx.send(hdl).await.unwrap();
+        });
+    }
+    // Collect results from the channel
+    for _ in 0 .. num_volumes {
+        results.push(rx.recv().await.unwrap());
+    }
+
+    results.iter().for_each(|r| {
+        if r.is_ok() {
+            success += 1;
+        } else {
+            failure += 1;
+        }
+    });
+    assert_eq!(success, 3);
+    assert_eq!(failure, 2);
 }
 
 async fn grpc_resize_volume_with_limit(
@@ -271,10 +369,12 @@ async fn grpc_resize_volume_with_limit(
     capacity: Option<u64>,
     expected_error: Option<ReplyErrorKind>,
 ) {
+    let vol_uuid = Uuid::new_v4();
+
     let volume = volume_client
         .create(
             &CreateVolume {
-                uuid: "de3cf927-80c2-47a8-adf0-95c486bdd7b7".try_into().unwrap(),
+                uuid: vol_uuid.try_into().unwrap(),
                 size: SIZE,
                 replicas: 2,
                 thin: false,
@@ -325,4 +425,29 @@ async fn grpc_resize_volume_with_limit(
                 .unwrap();
         }
     }
+}
+
+// Creates count number of volumes, and return the uuid of volume to be resized.
+async fn create_volumes(volume_client: &dyn VolumeOperations, count: u64) -> Vec<Uuid> {
+    let mut volumes = Vec::with_capacity(count as usize);
+    for _ in 0 .. count {
+        let vol_uuid = Uuid::new_v4();
+        let volume = volume_client
+            .create(
+                &CreateVolume {
+                    uuid: vol_uuid.try_into().unwrap(),
+                    size: SIZE,
+                    replicas: 1,
+                    thin: false,
+                    ..Default::default()
+                },
+                None,
+            )
+            .await
+            .unwrap();
+
+        volumes.push(volume.uuid().try_into().unwrap())
+    }
+
+    volumes
 }
