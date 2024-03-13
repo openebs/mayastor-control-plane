e793f882e983e21a85677cfc9356b021924f3fc3
sinhaashish <ashi.sinha.87@gmail.com>
2024-03-13 12:55:30 +0000

    feat(topology): add affinity-topology-key code to csi-driver
    Signed-off-by: sinhaashish <ashi.sinha.87@gmail.com>

--- control-plane/csi-driver/src/bin/controller/controller.rs
+++ control-plane/csi-driver/src/bin/controller/controller.rs
@@ -1068,6 +1068,8 @@ fn context_into_topology(context: &CreateParams) -> CreateVolumeTopology {
     let mut pool_inclusive_label_topology: HashMap<String, String> = HashMap::new();
     let mut node_inclusive_label_topology: HashMap<String, String> = HashMap::new();
     let mut node_exclusive_label_topology: HashMap<String, String> = HashMap::new();
+    let mut pool_affinity_label_topology: Vec<String> = Vec::<String>::new();
+    let mut node_affinity_label_topology: Vec<String> = Vec::<String>::new();
     pool_inclusive_label_topology.insert(String::from(CREATED_BY_KEY), String::from(DSP_OPERATOR));
     pool_inclusive_label_topology.extend(
         context
@@ -1080,6 +1082,13 @@ fn context_into_topology(context: &CreateParams) -> CreateVolumeTopology {
         context
             .publish_params()
             .pool_has_topology_key()
+            .clone()
+            .unwrap_or_default(),
+    );
+    pool_affinity_label_topology.extend(
+        context
+            .publish_params()
+            .pool_affinity_topology_key()
             .clone()
             .unwrap_or_default(),
     );
@@ -1104,14 +1113,23 @@ fn context_into_topology(context: &CreateParams) -> CreateVolumeTopology {
             .clone()
             .unwrap_or_default(),
     );
+    node_affinity_label_topology.extend(
+        context
+            .publish_params()
+            .node_affinity_topology_key()
+            .clone()
+            .unwrap_or_default(),
+    );
     CreateVolumeTopology::new(
         Some(models::NodeTopology::labelled(LabelledTopology {
             exclusion: node_exclusive_label_topology,
             inclusion: node_inclusive_label_topology,
+            affinitykey: node_affinity_label_topology,
         })),
         Some(PoolTopology::labelled(LabelledTopology {
             exclusion: Default::default(),
             inclusion: pool_inclusive_label_topology,
+            affinitykey: pool_affinity_label_topology,
         })),
     )
 }
