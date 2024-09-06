ff8ff8a7217697481ad60303f4f6025f391004ff
Tiago Castro <tiagolobocastro@gmail.com>
2024-09-06 21:39:18 +0100

    refactor(terraform): split master and worker
    Split master and worker name prefix.
    Parse master and worker cpu resources for lxd.
    Updates lxd to >=2.0.0.
    
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

--- terraform/cluster/mod/lxd/main.tf
+++ terraform/cluster/mod/lxd/main.tf
@@ -4,13 +4,16 @@ provider "lxd" {
 }
 
 variable "num_nodes" {}
+variable "master_nodes" {}
+variable "worker_nodes" {}
 variable "worker_memory" {}
 variable "worker_vcpu" {}
 variable "master_memory" {}
 variable "master_vcpu" {}
 variable "ssh_user" {}
 variable "ssh_key" {}
-variable "hostname_formatter" {}
+variable "master_fmt" {}
+variable "worker_fmt" {}
 variable "private_key_path" {}
 
 variable "pooldisk_size" {}
@@ -20,9 +23,13 @@ variable "image_path" {}
 variable "disk_size" {}
 variable "qcow2_image" {}
 
+variable "lxc_image" {}
+variable "lxc_cached_image" {}
+
 resource "lxd_cached_image" "ubuntu" {
-  source_remote = "ubuntu"
-  source_image  = "jammy/amd64"
+ source_remote = split(":", var.lxc_image)[0]
+ source_image  = split(":", var.lxc_image)[1]
+ count = var.lxc_cached_image ? 1 : 0
 }
 
 locals {
@@ -30,11 +37,12 @@ locals {
   # passes them to a template file to be filled in
   user_data = [
     for node_index in range(var.num_nodes) : templatefile("${path.module}/cloud_init.tmpl", {
-      ssh_user = var.ssh_user, ssh_key = var.ssh_key, hostname = format(var.hostname_formatter, node_index + 1)
+      ssh_user = var.ssh_user, ssh_key = var.ssh_key, hostname = node_index < var.master_nodes ? format(var.master_fmt, node_index + 1) : format(var.worker_fmt, node_index + 1 - var.master_nodes)
     })
   ]
   # likewise for networking
   network_config = templatefile("${path.module}/network_config.cfg", {})
+  use_ipv4 = true
 }
 
 resource "null_resource" "lxd_init" {
@@ -44,21 +52,26 @@ resource "null_resource" "lxd_init" {
 }
 
 resource "null_resource" "lxd_stop_force" {
+  triggers = {
+    master_nodes = var.master_nodes
+    master_fmt   = var.master_fmt
+    worker_fmt   = var.worker_fmt
+  }
   provisioner "local-exec" {
     when = destroy
     # todo: should use hostname_formatter
-    command = format("lxc stop ksnode-%d --force", count.index + 1)
+    command = format("lxc stop %s --force", count.index < self.triggers.master_nodes ? format(self.triggers.master_fmt, count.index + 1) : format(self.triggers.worker_fmt, count.index + 1 - self.triggers.master_nodes))
   }
   count = var.num_nodes
   depends_on = [
-    lxd_container.c8s
+    lxd_instance.c8s
   ]
 }
 
-resource "lxd_container" "c8s" {
+resource "lxd_instance" "c8s" {
   count     = var.num_nodes
-  name      = format(var.hostname_formatter, count.index + 1)
-  image     = lxd_cached_image.ubuntu.fingerprint
+  name      = count.index < var.master_nodes ? format(var.master_fmt, count.index + 1) : format(var.worker_fmt, count.index + 1 - var.master_nodes)
+  image     = var.lxc_cached_image ? lxd_cached_image.ubuntu[0].fingerprint : var.lxc_image
   ephemeral = false
 
   # be careful with raw.lxc it has to be key=value\nkey=value
@@ -74,9 +87,9 @@ resource "lxd_container" "c8s" {
   }
 
   limits = {
-    memory   = format("%dMiB", count.index == 0 ? var.master_memory : var.worker_memory)
+    memory   = format("%dMiB", count.index < var.master_nodes ? var.master_memory : var.worker_memory)
     # For the moment this doesn't as io-engine then can't set its core affinity...
-    # cpu      = count.index == 0 ? var.master_vcpu : var.worker_vcpu
+    # cpu      = count.index < var.master_nodes ? var.master_vcpu : var.worker_vcpu
   }
 
   device {
@@ -93,7 +106,7 @@ resource "lxd_container" "c8s" {
     connection {
       type        = "ssh"
       user        = var.ssh_user
-      host        = self.ip_address
+      host        = local.use_ipv4 ? self.ipv4_address : self.ipv6_address
       private_key = file(var.private_key_path)
     }
   }
@@ -106,22 +119,23 @@ resource "lxd_container" "c8s" {
 output "ks-cluster-nodes" {
   value = <<EOT
 [master]
-${lxd_container.c8s.0.name} ansible_host=${lxd_container.c8s.0.ip_address} ansible_user=${var.ssh_user} ansible_ssh_private_key_file=${var.private_key_path} ansible_ssh_common_args='-o StrictHostKeyChecking=no'
+${lxd_instance.c8s.0.name} ansible_host=${local.use_ipv4 ? lxd_instance.c8s.0.ipv4_address : lxd_instance.c8s.0.ipv6_address} ansible_user=${var.ssh_user} ansible_ssh_private_key_file=${var.private_key_path} ansible_ssh_common_args='-o StrictHostKeyChecking=no'
 
-[nodes]%{for ip in lxd_container.c8s.*~}
-%{if ip.name != "${format(var.hostname_formatter, 1)}"}${ip.name} ansible_host=${ip.ip_address} ansible_user=${var.ssh_user} ansible_ssh_private_key_file=${var.private_key_path} ansible_ssh_common_args='-o StrictHostKeyChecking=no'%{endif}
+[nodes]%{for ip in lxd_instance.c8s.*~}
+%{if ip.name != "${format(var.worker_fmt, 1)}"}${ip.name} ansible_host=${local.use_ipv4 ? ip.ipv4_address : ip.ipv6_address} ansible_user=${var.ssh_user} ansible_ssh_private_key_file=${var.private_key_path} ansible_ssh_common_args='-o StrictHostKeyChecking=no'%{endif}
 %{endfor~}
 EOT
 }
 
 output "node_list" {
-  value = lxd_container.c8s.*.ip_address
+  value = local.use_ipv4 ? lxd_instance.c8s.*.ipv4_address : lxd_instance.c8s.*.ipv6_address
 }
 
 terraform {
   required_providers {
     lxd = {
       source = "terraform-lxd/lxd"
+      version = ">= 2.0.0"
     }
   }
 }
