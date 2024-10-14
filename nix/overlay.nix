969ad175c7bae07a76c191e1b92ffc229c387531
Diwakar Sharma <diwakar.sharma@datacore.com>
2024-10-14 09:39:17 +0000

    feat: connect over tcp if initiator isn't rdma capable
    Signed-off-by: Diwakar Sharma <diwakar.sharma@datacore.com>

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -9,4 +9,5 @@ self: super: {
   control-plane = super.callPackage ./pkgs/control-plane { inherit allInOne incremental tag; };
   openapi-generator = super.callPackage ./pkgs/openapi-generator { };
   xfsprogs_5_16 = (import (super.sources).nixpkgs-22_05 { }).xfsprogs;
+  rdmacore = (import (super.sources).nixpkgs-22_05 { }).rdma-core;
 }
