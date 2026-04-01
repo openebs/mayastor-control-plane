31dc3f78871889276eb74cde1a232c5300442637
Tiago Castro <tiagolobocastro@gmail.com>
2026-04-01 10:05:00 +0100

    build: add missing feature cfg on dsp operator for openapi
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

--- k8s/operators/src/pool/diskpool/crd/v1beta3.rs
+++ k8s/operators/src/pool/diskpool/crd/v1beta3.rs
@@ -353,6 +353,7 @@ pub(crate) enum DspCondition {
 }
 
 impl DiskPoolStatus {
+    #[cfg(feature = "openapi")]
     fn inferred_status(pool: &Pool) -> PoolStatus {
         if let Some(ref diag) = pool.diag {
             return diag.status.into();
