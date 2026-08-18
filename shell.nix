5ea285c0e960052eef14b4b672e5de50ac885768
Tiago Castro <tiagolobocastro@gmail.com>
2026-08-19 00:50:08 +0100

    test: test things
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

--- shell.nix
+++ shell.nix
@@ -20,7 +20,7 @@ let
   rust = rust_chan.${rust-profile}.overrideAttrs (oldAttrs: {
     # don't propagate any build inputs - this allows us to set cc in stdenv below
     propagatedBuildInputs = [ ];
-    depsHostHostPropagated = [ ];
+    depsHostHostPropagated = [ clang ];
     depsTargetTargetPropagated = [ ];
   });
   usePreCommit = builtins.getEnv "IN_NIX_SHELL" == "impure" && builtins.getEnv "CI" != "1";
