4f6bb2ff3a8a2e805cc5d330b7b081fb7ae32d4d
Abhilash Shetty <abhilash.shetty@datacore.com>
2026-09-03 10:46:45 +0000

    feat(pool-drain): adding rest change for drain api
    Signed-off-by: Abhilash Shetty <abhilash.shetty@datacore.com>

--- control-plane/stor-port/src/types/v0/transport/pool.rs
+++ control-plane/stor-port/src/types/v0/transport/pool.rs
@@ -663,7 +663,9 @@ impl From<Pool> for models::Pool {
         let (spec, meta) = match def {
             None => (None, None),
             Some(def) => {
-                let meta = models::PoolMeta::new_all(def.replica_count, def.snapshot_count);
+                let drain: Option<models::PoolDrainRecord> =
+                    def.persisted_metadata.drain_record.into_opt();
+                let meta = models::PoolMeta::new_all(def.replica_count, def.snapshot_count, drain);
                 (Some(def.spec), Some(meta))
             }
         };
